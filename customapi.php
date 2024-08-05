7be7d64e3ae2711c476d4c37f11bc8e034f7f1a6
galohot <114443831+galohot@users.noreply.github.com>
2024-08-05 09:47:41 +0700

    stable work

--- customapi.php
+++ customapi.php
@@ -1,78 +1,124 @@
 <?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.customapi
+ */
+
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Response\JsonResponse;
+use Joomla\CMS\Plugin\CMSPlugin;
 
 class PlgSystemCustomapi extends CMSPlugin
 {
+    protected $app;
+
     public function onAfterInitialise()
     {
-        $app = Factory::getApplication();
-        $input = $app->input;
-
-        // Check if this is a request to the custom API endpoint
-        if ($input->get('option') !== 'com_customapi') {
-            return;
-        }
-
-        // Authenticate request
+        $input = Factory::getApplication()->input;
+        $option = $input->get('option', '', 'cmd');
         $apiKey = $this->params->get('api_key');
-        $requestKey = $input->get('api_key');
-        if ($apiKey !== $requestKey) {
-            $this->outputJson(null, JText::_('PLG_SYSTEM_CUSTOMAPI_INVALID_API_KEY'), true);
-            $app->close();
+
+        if ($option === 'com_customapi')
+        {
+            $providedApiKey = $input->get('api_key', '', 'string');
+            $tableName = $input->get('table', '', 'string'); // Get the table name from URL parameters
+            $fields = $input->get('fields', '*', 'string'); // Get the fields from URL parameters, default to '*'
+            $format = $input->get('format', 'json', 'string'); // Get the response format from URL parameters (default to json)
+
+            if ($apiKey !== $providedApiKey)
+            {
+                $this->outputResponse(null, JText::_('JERROR_ALERTNOAUTHOR'), $format);
+                Factory::getApplication()->close();
+            }
+
+            if (empty($tableName))
+            {
+                $this->outputResponse(null, JText::_('JERROR_TABLE_NAME_NOT_PROVIDED'), $format);
+                Factory::getApplication()->close();
+            }
+
+            try
+            {
+                $db = Factory::getDbo();
+
+                if ($fields === '*') {
+                    // Fetch all columns if fields is '*'
+                    $columnsQuery = $db->getQuery(true)
+                        ->select('COLUMN_NAME')
+                        ->from('INFORMATION_SCHEMA.COLUMNS')
+                        ->where('TABLE_SCHEMA = DATABASE()')
+                        ->where('TABLE_NAME = ' . $db->quote($tableName));
+
+                    $db->setQuery($columnsQuery);
+                    $columns = $db->loadColumn();
+                    $fields = implode(',', array_map([$db, 'quoteName'], $columns));
+                }
+
+                $query = $db->getQuery(true)
+                    ->select($fields)
+                    ->from($db->quoteName($tableName));
+
+                $db->setQuery($query);
+                $results = $db->loadAssocList();
+
+                $this->outputResponse($results, null, $format);
+            }
+            catch (Exception $e)
+            {
+                $this->outputResponse(null, $e->getMessage(), $format);
+            }
+
+            Factory::getApplication()->close();
         }
-
-        // Get table name from parameters
-        $tableName = $this->params->get('table_name');
-        if (empty($tableName)) {
-            $this->outputJson(null, 'Table name not specified', true);
-            $app->close();
-        }
-
-        // Determine format
-        $format = $input->getWord('format', 'json');
-
-        // Get selected fields from query parameters
-        $fields = $input->getString('fields', '*');
-
-        // Fetch data from the specified table
-        $db = Factory::getDbo();
-        $query = $db->getQuery(true)
-            ->select($fields)
-            ->from($db->quoteName($tableName));
-        $db->setQuery($query);
-        $results = $db->loadAssocList();
-
-        // Output data
-        if ($format === 'csv') {
-            $this->outputCsv($results);
-        } else {
-            $this->outputJson($results);
-        }
-
-        $app->close();
     }
 
-    private function outputJson($data, $message = null, $error = false)
+    private function outputResponse($data, $error = null, $format = 'json')
     {
-        header('Content-Type: application/json');
-        echo json_encode($data);
+        if ($format === 'csv')
+        {
+            $this->outputCsv($data, $error);
+        }
+        else
+        {
+            $this->outputJson($data, $error);
+        }
     }
 
-    private function outputCsv($data)
+    private function outputCsv($data, $error = null)
     {
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment;filename=export.csv');
+        header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename="export.csv"');
         $output = fopen('php://output', 'w');
-        if (count($data) > 0) {
-            fputcsv($output, array_keys($data[0]));
-            foreach ($data as $row) {
-                fputcsv($output, $row);
+
+        if ($error)
+        {
+            fputcsv($output, [$error]);
+        }
+        else
+        {
+            if (!empty($data))
+            {
+                fputcsv($output, array_keys(reset($data))); // Output header row
+                foreach ($data as $row)
+                {
+                    fputcsv($output, $row);
+                }
             }
         }
+
         fclose($output);
+    }
+
+    private function outputJson($data, $error = null)
+    {
+        if ($error)
+        {
+            echo json_encode(['error' => $error]);
+        }
+        else
+        {
+            echo json_encode($data);
+        }
     }
 }
